21c54ffe8f825421f39c5abcba86da46c9f21d3b
Galaceon <antoniogarcialtr@gmail.com>
2026-03-07 18:03:05 +0100

    adding items to state

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,42 @@
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
-import { useState, useEffect } from "react"
+import { useState } from "react"
+import { db } from './data/db'
 
 function App() {
     
-    const [auth, setAuth] = useState(false)
+    const [data, setData] = useState(db)
 
-    //Efect
-    useEffect(() => {
-        if(auth) {
-            console.log('Autentificado')
-        }
-    }, [auth])
-    
-    setTimeout(() => {
-        setAuth(true)
-    }, 3000)
+    return (
+        <>
+            <Header />
 
-  return (
-    <>
+            <main className="container-xl mt-5">
+                <h2 className="text-center">Nuestra Colección</h2>
 
-    <Header />
+                <div className="row mt-5">
 
-    <main className="container-xl mt-5">
-        <h2 className="text-center">Nuestra Colección</h2>
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
+                    <Guitar />
 
-        <div className="row mt-5">
-
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
-            <Guitar />
-
-        </div>
-    </main>
+                </div>
+            </main>
 
 
-    <footer className="bg-dark mt-5 py-5">
-        <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
-        </div>
-    </footer>
-    </>
-  )
+            <footer className="bg-dark mt-5 py-5">
+                <div className="container-xl">
+                    <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+                </div>
+            </footer>
+        </>
+    )
 }
 
 export default App
