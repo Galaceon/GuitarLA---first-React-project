bd34ce457a55dba1cae357fe20e25d06210fcdfb
Galaceon <antoniogarcialtr@gmail.com>
2026-03-09 05:40:27 +0100

    showing info about each guitar

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,10 @@ function App() {
                 <h2 className="text-center">Nuestra Colección</h2>
 
                 <div className="row mt-5">
-                    {data.map(() => 
-                        <Guitar 
-                            price={100}
-                            auth={true}
+                    {data.map((guitar) => 
+                        <Guitar
+                            key={guitar.id}
+                            guitar={guitar}
                         />
                     )}
                     
